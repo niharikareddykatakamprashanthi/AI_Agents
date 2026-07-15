230e212558aa517ff8a13b35db65f656f500a107
niharikareddykatakamprashanthi <niharikareddykatakamprashanthi@gmail.com>
2026-07-15 19:09:43 -0400

    code updated for inbuilt tool calling

---
+++ scratch_test.py
@@ -0,0 +1,7 @@
+from langgraph.graph import StateGraph, START, END
+
+# Put your cursor after the dot below and press Ctrl+Space.
+# If a suggestion list appears here (in a .py file) but NOT in the notebook,
+# the problem is notebook-specific. If nothing appears here either,
+# the language server (cursorpyright) isn't activating.
+StateGraph
